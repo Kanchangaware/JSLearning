aa58bcf98d4ea35feb998e355dff5379085c1611
Kanchangaware <kanchangaware291@gmail.com>
2023-02-23 19:11:43 +0530

    datatype.js
    datatype.js

---
+++ 03_datatypes.js
@@ -0,0 +1,30 @@
+var myName="Kanchan Gaware";
+var fatherName="shaligram ramchandr Gaware";
+var age=31;
+var weight=82.5;
+var isMarried= true;
+var isLasy= false;
+var typeOfMyName = typeof myName;
+console.log("Typeof variable myName: ",typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("TypeOf  age is :", age);
+console.log("TypeOf  weight is :", weight);
+console.log("TypeOf  isMarried is :", isMarried);
+console.log("TypeOf  isLasy is :", isLasy);
+
+var graduation;
+console.log("tvalue Of variable graduation is: ", typeof graduation);
+console.log("TypeOf  graduation is :", graduation);
+
+var city= null;
+console.log("value Of variable city is: ", typeof city);
+console.log("TypeOf  city is :", city);
+age="34";
+console.log(typeof age);
+age=25;
+console.log(typeof age);
+age="true";
+console.log(typeof age);
+var alphabet= "K";
+console.log(typeof alphabet);
+
