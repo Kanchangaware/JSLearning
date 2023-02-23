293b24d9893a620381258d05468dd715de8730bc
Kanchangaware <kanchangaware291@gmail.com>
2023-02-23 19:10:39 +0530

    datatype.js
    variable.js

---
+++ 02_variable_assign.js
@@ -0,0 +1,30 @@
+console.log("****Step 1*****");
+myFullName = "kanchan Shaligram Gaware";
+console.log("My Full Name is:", myFullName);
+console.log("*****Step 2*****");
+var myLove = "I Love Only JS";
+console.log(myLove);
+console.log("******Step 3******");
+var firstName = "Kanchan";
+var lastName = "Gaware";
+var age = "32";
+var address = "At Post kothali Tq Motala Dist Buldhana";
+var pincode = "443103";
+var gender = "Female";
+var isMarried = "No";
+console.log(
+  "My First Name is:",
+  firstName,
+  "*Last Name:",
+  lastName,
+  "My age is:",
+  age,
+  "*Address:",
+  address,
+  "Pincode:",
+  pincode,
+  "Gender:",
+  gender,
+  "Is Married",
+  isMarried
+);
