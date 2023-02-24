66ba09729c1e52f5cb14f4980f21471b603277ea
Kanchangaware <kanchangaware291@gmail.com>
2023-02-24 10:52:19 +0530

    01_helloWorld.js
    01_helloWorld.js

---
+++ 01_helloWorld.js
@@ -0,0 +1,23 @@
+// Personal Details
+console.log("Kanchan Gaware");
+console.log(31);
+console.log("Pune");
+
+var full_Name = "Kanchan Gaware";
+console.log(full_Name);
+console.log(full_Name);
+
+var city; //variable Declaration
+console.log(city);
+city = "pune"; //variable initialization
+console.log(city);
+city = "Buldhana";
+console.log(city);
+var age = "31"; //variable declaration and initialization in same line
+age = 32;
+age = 33;
+
+console.log(age);
+
+var university = "SGBAU"; //Variable Declaration and initialization in same line
+console.log("My University name is:", university);
