05e4caf3ba1931aa96eb29d5665d23d8f234a5a2
Kanchangaware <kanchangaware291@gmail.com>
2023-02-26 23:35:49 +0530

    04_stringbasicAssig.js
    04_stringbasicAssig.js

---
+++ 04_stringBasicAssig.js
@@ -0,0 +1,20 @@
+console.log("****Step1****");
+function stringBasics() {
+    console.log("My dream company is: TCS");
+  }
+  stringBasics();
+  var myHobbies1= "Playing Game";
+  var myHobbies2= "Watching Movies";
+  var myHobbies3= "reading Books";
+  console.log("My Hobbies are:", myHobbies1, myHobbies2, myHobbies3);
+  console.log("****Step3****");
+  console.log("Totle Number of charector into myHobbies1: 9", typeof myHobbies1);
+  console.log("Totle Number of charector into myHobbies2: 6", typeof myHobbies2);
+  console.log("Totle Number of charector into myHobbies3: 12", typeof myHobbies3);
+  console.log("Sum Totle Number of charector of all three hobbies:", 27);
+
+
+  
+
+
+
