a5b8cbe5d7e70dc79531df46b924c7a57d509d68
Kanchangaware <kanchangaware291@gmail.com>
2023-02-26 23:31:40 +0530

    03_swapVariable
    03_swapVariable

---
+++ 03_swapVariable.js
@@ -0,0 +1,16 @@
+console.log("******Swapping Two Variables******");
+var num1="100";
+var num2="200";
+
+var str1 = "Sweety"; //Cutey
+var str2 = "cutey";   //Sweety
+
+
+console.log("Before Swapping:", num1, num2);
+var temp=num1;
+num1=num2;
+temp=num2;
+console.log("After Swapping:", num1, num2);
+console.log("Before Swapping:", str1, str2);
+console.log("After Swapping:", str1, str2);
+
