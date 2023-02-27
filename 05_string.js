cdd53945a6b63343eafe8393814c283adb4522e9
Kanchangaware <kanchangaware291@gmail.com>
2023-02-27 19:00:12 +0530

    05_string.js
    05_string.js

---
+++ 05_string.js
@@ -0,0 +1,6 @@
+var greet = "Good Morning";
+console.log(typeof greet);
+console.log("Total number chars available into string variable - greet");
+var greetLength = greet.length;
+console.log(greetLength);
+
