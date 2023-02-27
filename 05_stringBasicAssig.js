4f510fb2ba7a6773ed68a79b05309b6afef030e9
Kanchangaware <kanchangaware291@gmail.com>
2023-02-27 18:59:37 +0530

    05_stringBasicsAssig.js
    05_stringBasicsAssig.js

---
+++ 05_stringBasicAssig.js
@@ -0,0 +1,25 @@
+console.log("****Step1****");
+var greet = "My dream company is: TCS";
+    console.log("My dream company is: TCS");
+    console.log("****Step4****");
+
+  var hobbies= "Dacing";
+  var hobbies1= "Reading";
+  var hobbies2= "Tracking";
+  console.log(hobbies, hobbies1, hobbies2);
+  console.log("****Step3****");
+
+  var hobbiesLength= hobbies.length;
+  console.log("Total length of string hobbies is:", hobbiesLength);
+  var hobbies1Length= hobbies1.length;
+  console.log("Total length of string hobbies1 is:", hobbies1Length);
+  var hobbies2Length= hobbies2.length;
+  console.log("Total length of string hobbies2 is:", hobbies2Length);
+  console.log("sum of the total Number of characters that is available ln hobbies hobbies1, hobbies2:", hobbies,  hobbies1, hobbies2);
+  
+
+
+  
+
+
+
