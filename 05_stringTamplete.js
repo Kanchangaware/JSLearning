83e8e0046b83897f772c83dbb86604317508e23a
Kanchangaware <kanchangaware291@gmail.com>
2023-03-31 15:57:47 +0530

    05_stringTamplete.js
    05_stringTamplete.js

---
+++ 05_stringTamplete.js
@@ -0,0 +1,9 @@
+//``=it is called back tick
+console.log(`You are "champ" man...`);
+
+var firstName= "Mohit";
+var lastName= "Sharma";
+var city = "Pune";
+
+console.log("My first name", firstName, "Last name", lastName, "city",city);
+console.log(`My first name: ${firstName}last name : ${lastName} city : ${city} `);
