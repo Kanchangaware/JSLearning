9a392e9e43092d5c484eadf2dba092a973f203fa
Kanchangaware <kanchangaware291@gmail.com>
2023-03-31 22:57:50 +0530

    06_operater.Ass
    06_operater.Ass

---
+++ .vscode/06_operater.js
@@ -0,0 +1,61 @@
+
+
+console.log(`======Arithmatic Operator=====`);
+var num1 = 10;
+var num2 = 2;
+var result = num1 + num2;  //Addition
+console.log(`Addition is ${result}`);
+
+result=num1-num2;
+console.log(`subtraction is ${result}`);
+
+result= num1*num2;
+console.log(`multiplication is : ${result}`);
+
+result= num1/num2;
+console.log(`division is: ${result}`);
+
+//square =10 *10
+result= num1**num2;
+console.log(`Exponetial is : ${result}`);
+
+//cube =10*3
+result = num1**3;
+console.log(`Cube is :${result}`);
+result=num1/3;
+console.log(`Division is: ${result}`);
+result=num1%3
+console.log(`Reminder is: ${result}`);
+
+var num =10;  //compound addition +=
+num+=20;  //num= num+20
+console.log(`Compound addition += ${num}`);
+
+console.log(`=====caparision operators=====`);
+var num3 ="10";
+var num4 = 10;
+console.log(num3==num4,);  // 10==10
+console.log(num3===num4);  //Strictly equal 
+
+console.log(num3!=num4); //!= is not equal 
+console.log(10>20); //> is  greater than
+console.log(10<111); //> is  less than
+
+var marks=35;
+console.log(marks>=35);
+// is marks greater than 35
+// Is marks greater than equal 35
+
+//  Less the equal
+
+
+
+
+
+
+
+
+
+
+
+
