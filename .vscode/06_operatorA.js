e7916a03cddd1785a58529b20f392dd9df7b8aa7
Kanchangaware <kanchangaware291@gmail.com>
2023-04-01 15:01:41 +0530

    06_operatorA
    06_operatorA

---
+++ .vscode/06_operatorA.js
@@ -0,0 +1,16 @@
+
+function squareOfWordLength(words){
+    var num1=10;
+    var result =num1**2;
+    console.log(`The word is:${words} and square length:${result}`);
+}squareOfWordLength("Google chrome");
+var myLang="JavaScript";
+var myServer = "Google Chrome";
+var myEmp="Developer Smart";
+console.log(`${myLang},${myServer},${myEmp}`);
+function myDeveloper() {
+    console.log("I am Angular Developer");
+}
+myDeveloper();
+var myDeveloperLength=myDeveloper.length;
+console.log("length of giver String:", myDeveloper.length);
